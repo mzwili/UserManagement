b701efc17c10deb9b6a925c9a105ada7468ad6fc
Mzwili <mthiyanemzwili@gmail.com>
2024-09-16 19:36:34 +0200

    added get all users end point

--- src/main/java/com/mzwiliapp/userManage/controller/UserController.java
+++ src/main/java/com/mzwiliapp/userManage/controller/UserController.java
@@ -3,9 +3,12 @@ package com.mzwiliapp.userManage.controller;
 import com.mzwiliapp.userManage.model.User;
 import com.mzwiliapp.userManage.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -13,8 +16,13 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/user")
+    @PostMapping("/addUser")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/allUsers")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
