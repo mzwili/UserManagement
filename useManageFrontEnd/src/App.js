125070bedf8edfa115e243a3065405a4b21dd0a1
Mzwili <mthiyanemzwili@gmail.com>
2024-09-17 15:11:10 +0200

    created frontend package

--- useManageFrontEnd/src/App.js
+++ useManageFrontEnd/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
 
