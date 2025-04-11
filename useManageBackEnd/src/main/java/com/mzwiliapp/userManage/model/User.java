255c44ee1eb7e989aa7a02190bb23728ea7a275d
Mzwili <mthiyanemzwili@gmail.com>
2025-04-11 16:34:47 +0200

    added xata configurations

--- useManageBackEnd/src/main/java/com/mzwiliapp/userManage/model/User.java
+++ useManageBackEnd/src/main/java/com/mzwiliapp/userManage/model/User.java
@@ -1,16 +1,15 @@
 package com.mzwiliapp.userManage.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String name;
